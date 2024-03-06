51b2b94cf00e2bef16b1d0ebb13cc0f960542abe
Konrad1w <komrad1214@gmail.com>
2024-03-06 09:06:28 +0100

    delegates mini-proj done earlier

---
+++ Delegates/Delegates/Parlament.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw2Delegaty
+{
+	public delegate void Notify();
+	public delegate void Notify1(bool glos);
+	class Parlament
+	{
+		public event Notify poczatekGlosowaniaEvent;
+		public event Notify koniecGlosowaniaEvent;
+		public List<Parlamentarzysta> parlamentarzysci;
+		public string tematGlosowania;
+		public int liczbaParlamentarzystow;
+		public int zaGlosy;
+		public int przeciwGlosy;
+		public Parlament(string tematGlosowania, int liczbaParlamentarzystow)
+		{
+			parlamentarzysci = new List<Parlamentarzysta>(liczbaParlamentarzystow);
+			this.tematGlosowania = tematGlosowania;
+			this.liczbaParlamentarzystow = liczbaParlamentarzystow;
+			zaGlosy = 0;
+			przeciwGlosy = 0;
+			for (int i = 0; i < liczbaParlamentarzystow; i++)
+			{
+				parlamentarzysci.Add(new Parlamentarzysta(i, this));
+				poczatekGlosowaniaEvent += parlamentarzysci[i].SetMozeGlosowacTrue;
+				koniecGlosowaniaEvent += parlamentarzysci[i].SetMozeGlosowacFalse;
+			}
+		}
+		public void StartGlosowania(string temat)
+		{
+			if (tematGlosowania == temat)
+			{
+				OnPoczatekGlosowania();
+				Console.WriteLine("Glosowanie rozpoczelo sie na temat: " + temat);
+			}
+		}
+		void OnPoczatekGlosowania()
+		{
+			poczatekGlosowaniaEvent?.Invoke();
+		}
+		void OnKoniecGlosowania()
+		{
+			koniecGlosowaniaEvent?.Invoke();
+		}
+		public void KoniecGlosowania()
+		{
+			OnKoniecGlosowania();
+			OstatnieGlosowanie();
+			Console.WriteLine("Koniec glosowania");
+		}
+		private void OstatnieGlosowanie()
+		{
+			Console.WriteLine("Ostatnie glosowanie na temat:" + tematGlosowania + " liczba glosow za: " + zaGlosy + " liczba glosow przeciw: " + przeciwGlosy);
+		}
+		public void glosParlamentarzysty(bool glos)
+		{
+			if (glos)
+				zaGlosy++;
+			else
+				przeciwGlosy++;
+		}
+
+	}
+	class Parlamentarzysta
+	{
+		bool mozeGlosowac;
+		int numer;
+		bool glos;
+		public event Notify1 oddanieGlosuEvent;
+		public Parlamentarzysta(int numer, Parlament p)
+		{
+			mozeGlosowac = false;
+			this.numer = numer;
+			oddanieGlosuEvent += p.glosParlamentarzysty;
+		}
+		public void SetMozeGlosowacTrue()
+		{
+			mozeGlosowac = true;
+		}
+		public void SetMozeGlosowacFalse()
+		{
+			mozeGlosowac = false;
+		}
+		public void Glosuj()
+		{
+			if (mozeGlosowac)
+			{
+				Random rand = new Random();
+				glos = rand.Next(2) == 0;
+				OnOddanieGlosu(glos);
+				SetMozeGlosowacFalse();
+				Console.Write("Parlamentarzysta " + numer + " oddaje glos na: " + glos);
+			}
+			else
+			{
+				Console.WriteLine("Nie mozesz oddac glosu, bo juz to zrobiles lub glosowanie sie nie odbywa");
+			}
+		}
+		void OnOddanieGlosu(bool b)
+		{
+			oddanieGlosuEvent?.Invoke(b);
+		}
+
+	}
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,46 @@
+﻿using Cw2Delegaty;
+using System;
+class Cw2
+{ 
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe parlamentarzystow");
+            int liczbaParlamentarzystow = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj temat glosowania");
+            string tematGlosowania = Console.ReadLine();
+            Parlament parlament = new Parlament(tematGlosowania,liczbaParlamentarzystow);
+            while (true) 
+            {
+                string command = Console.ReadLine();
+                if(command.StartsWith("POCZĄTEK "))
+                {
+                    parlament.StartGlosowania(command.Substring(command.LastIndexOf(" ") + 1));
+                }
+                else if(command.StartsWith("GŁOS "))
+                {
+                    int numerParlamenatrzysty = -1;
+                    bool parsable = int.TryParse(command.Substring(command.LastIndexOf(" ") + 1), out numerParlamenatrzysty);
+                    if (parsable && numerParlamenatrzysty < parlament.liczbaParlamentarzystow)
+                    {
+                        parlament.parlamentarzysci[numerParlamenatrzysty].Glosuj();
+                    }
+                    else if (parsable)
+                        Console.WriteLine("Niepoprawny numer palamentarzysty");
+                    else
+                        Console.WriteLine("Wpisz numer parlamentarzysty po tekscie \"GŁOS\"");
+                }
+                else if(command=="KONIEC")
+                {
+                    parlament.KoniecGlosowania();
+                }
+                else
+                {
+                    Console.WriteLine("Nieznana komenda. Do wyboru masz komendy: POCZĄTEK, GŁOS, KONIEC");
+                }
+
+            }
+        }
+    }
+}
